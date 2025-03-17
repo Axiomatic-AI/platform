20175ba4a13244f299cc843a0865db7b4b537392
Jacob Windsor <me@jcbwndsr.com>
2025-03-17 18:34:06 +0100

    wip: loading

--- app/pic-designer/page.tsx
+++ app/pic-designer/page.tsx
@@ -2,22 +2,21 @@
 
 import { ChatInput } from './components/ChatInput';
 import { MessagesArea } from './components/MessagesArea';
-import { FractalTree } from './components/FractalTree';
+import { Loading } from './components/Loading';
 import { usePicDesigner } from './hooks/usePicDesigner';
 
 export default function PICDesigner() {
   const { queries, isLoading, error, isInitialState, handleSendMessage } = usePicDesigner();
 
   return (
-    <div className="flex flex-col w-full h-full">
+    <div className="flex flex-col max-w-4xl w-full h-full">
       {!isInitialState ? (
         <div className="flex-1 min-h-0 flex flex-col">
           <div className="flex-1 overflow-y-auto">
-            {isLoading ? 
-              <FractalTree /> : 
+            {true ? 
+              <Loading /> : 
               <MessagesArea 
                 queries={queries}
-                isLoading={isLoading}
                 error={error}
               />
             }
@@ -28,7 +27,7 @@ export default function PICDesigner() {
         </div>
       ) : (
         <div className="flex-1 flex items-center justify-center">
-          <div className="max-w-4xl mx-auto w-full px-4">
+          <div className="mx-auto w-full px-4">
             <h1 className="font-bold text-gray-800 dark:text-white mb-2">
               What PIC would you like to build today?
             </h1>
