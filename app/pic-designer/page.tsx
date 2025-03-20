41a71d3ca872b616c7ded759626d9b63e366d14c
Jacob Windsor <me@jcbwndsr.com>
2025-03-20 19:06:47 +0100

    Add a new thread button

--- app/pic-designer/page.tsx
+++ app/pic-designer/page.tsx
@@ -30,6 +30,11 @@ export default function PICDesigner() {
     setCurrentQueryIndex(0);
   };
 
+  const handleNewThread = () => {
+    setCurrentThreadId(undefined);
+    setCurrentQueryIndex(0);
+  };
+
   const getPreviousCode = () => {
     if (!thread?.queries) return undefined;
     
@@ -55,7 +60,7 @@ export default function PICDesigner() {
 
   return (
     <div className="flex h-full w-full">
-      <div className="flex-0">
+      <div className="flex-0 flex flex-col">
         <HistorySidebar 
           onDeleteAll={handleDeleteAllThreads}
           onThreadSelect={handleThreadSelect}
@@ -64,6 +69,15 @@ export default function PICDesigner() {
           isLoading={isThreadListLoading}
         />
       </div>
+      <button
+          onClick={handleNewThread}
+          className="m-4 w-10 h-10 flex items-center justify-center bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-200 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors"
+          title="New Thread"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+          </svg>
+        </button>
       <div className="flex-1 flex flex-col max-w-4xl h-full mx-auto">
         {thread || isPostQueryLoading ? (
           <div className="flex-1 min-h-0 flex flex-col">
