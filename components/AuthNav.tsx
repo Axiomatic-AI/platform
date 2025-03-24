4cd7a936975d78d36c5c4daf962e87b550a0d618
Jacob Windsor <me@jcbwndsr.com>
2025-03-24 10:10:45 +0100

    Make the sidebar more compact

--- components/AuthNav.tsx
+++ components/AuthNav.tsx
@@ -14,7 +14,7 @@ export default function AuthNav() {
 
   return (
     <div className="w-full">
-      <div className="flex items-center px-2 py-3 border-gray-200 dark:border-gray-700">
+      <div className="flex flex-col items-center px-2 py-3 border-gray-200 dark:border-gray-700">
         <div className="flex-shrink-0">
           {user.picture && (
             <Image
@@ -26,24 +26,16 @@ export default function AuthNav() {
             />
           )}
         </div>
-        <div className="ml-3">
-          <p className="text-sm font-medium text-gray-900 dark:text-white">
-            {user.name}
-          </p>
-          <p className="text-xs text-gray-500 dark:text-gray-400">
-            {user.email}
-          </p>
-        </div>
       </div>
       <a
         href="/auth/logout"
-        className="group flex items-center w-full px-2 py-2 text-sm font-medium rounded-md text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 hover:text-gray-900 dark:hover:text-white"
+        className="group flex flex-col items-center w-full px-2 py-2 text-xs font-medium rounded-md text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 hover:text-gray-900 dark:hover:text-white"
       >
         <LogoutIcon
-          className="mr-3 flex-shrink-0 h-6 w-6 text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+          className="mb-1 flex-shrink-0 h-5 w-5 text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
           aria-hidden="true"
         />
-        Logout
+        <span>Logout</span>
       </a>
     </div>
   );
