4365b0e9f03160b2cb513c754fc22b854674e83c
Jacob Windsor <jwindsor@axiomatic-ai.com>
2025-03-26 13:32:42 +0100

    position toolbar

--- app/pic-designer/components/ChatInput.tsx
+++ app/pic-designer/components/ChatInput.tsx
@@ -117,7 +117,7 @@ export function ChatInput({
               disabled={isTyping}
             >
               <PaperClipIcon />
-              <span className="absolute -bottom-8 left-1/2 -translate-x-1/2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
+              <span className="absolute -top-8 left-1/2 -translate-x-1/2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
                 Attach file
               </span>
             </button>
