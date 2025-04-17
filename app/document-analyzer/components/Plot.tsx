a7cc8493077e4c2d39d2c2d540d07c2bfe10e698
Jacob Windsor <jwindsor@axiomatic-ai.com>
2025-04-17 23:04:33 +0200

    Improve colour palette

--- app/document-analyzer/components/Plot.tsx
+++ app/document-analyzer/components/Plot.tsx
@@ -4,20 +4,32 @@ interface PlotProps {
     data: PlotPointsResponse;
 }
 
+const COLOR_PALETTE = [
+    'bg-blue-500',
+    'bg-orange-500',
+    'bg-green-500',
+    'bg-red-500',
+    'bg-purple-500',
+    'bg-amber-500',
+    'bg-pink-500',
+    'bg-gray-500',
+    'bg-emerald-500',
+    'bg-cyan-500',
+];
+
 export function Plot({ data }: PlotProps) {
     const { extractedSeries } = data;
 
     return (
         <div className="relative w-full h-full">
-            {extractedSeries.map((series) => (
+            {extractedSeries.map((series, seriesIndex) => (
                 series.points.map((point, index) => (
                     <div
                         key={`${series.id}-${index}`}
-                        className="absolute w-1.5 h-1.5 rounded-full transform -translate-x-1/2 -translate-y-1/2"
+                        className={`absolute w-1.5 h-1.5 rounded-full transform -translate-x-1/2 -translate-y-1/2 ${COLOR_PALETTE[seriesIndex % COLOR_PALETTE.length]}`}
                         style={{
                             left: `${point.percentageCoordX * 100}%`,
                             bottom: `${point.percentageCoordY * 100}%`,
-                            backgroundColor: `rgb(${series.color.join(',')})`,
                         }}
                     />
                 ))
