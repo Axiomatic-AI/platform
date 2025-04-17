14f927783350108cc2e7268a711eed0aa18a4dcf
Jacob Windsor <jwindsor@axiomatic-ai.com>
2025-04-17 22:56:23 +0200

    Fix coordinates

--- app/document-analyzer/components/Plot.tsx
+++ app/document-analyzer/components/Plot.tsx
@@ -15,8 +15,8 @@ export function Plot({ data }: PlotProps) {
                         key={`${series.id}-${index}`}
                         className="absolute w-2 h-2 rounded-full transform -translate-x-1/2 -translate-y-1/2"
                         style={{
-                            left: `${(origin[0] + point.percentageCoordX * xAxisLen) * 100}%`,
-                            top: `${(1 - (origin[1] + point.percentageCoordY * yAxisLen)) * 100}%`,
+                            left: `${point.percentageCoordX * 100}%`,
+                            bottom: `${point.percentageCoordY * 100}%`,
                             backgroundColor: `rgb(${series.color.join(',')})`,
                         }}
                     />
